87f4fa62e3734f024359cb2de9718925963674e4
pavan-sai-korlapati <pavan-sai.korlapati@capgemini.com>
2022-05-16 23:11:18 +0530

    16/5/2022

--- src/main/java/com/recipe/RecipeApplication.java
+++ src/main/java/com/recipe/RecipeApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
